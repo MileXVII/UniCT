342f3a8bbd48bb4d383e62f2f95cb979d5d1f348
MileXVII <RNRMHL05P05L042I@studium.unict.it>
2025-06-14 17:27:15 +0200

    Update bubblesort.s

--- Architettura/esercizi/bubblesort.s
+++ Architettura/esercizi/bubblesort.s
@@ -1,28 +1,37 @@
-size    dcd     10
-array   dcd     4, 6, 18, 8, 3, 9, 91, 72, 14 ,22
+size           dcd     10
+array          dcd     9,4,5,28,43,1,8,3,4,3
 
-        mov     r1, #size
-        ldr     r0, [r1]
-        mov     r1, #array
-loop    
-        cmp     r0, #1
-        ble     fine
-        sub     r0, r0, #1
-        mov     r2, r1 ;puntatore corrente inizio array
-        mov     r9, r0 ;control loop
-control 
-        cmp     r9, #0
-        beq     loop
-        ldr     r3, [r2] ;el. corrente
-        ldr     r4, [r2, #4] ;el. successivo
-        cmp     r3, r4
-        ble     skip
-        ;scambio
-        str     r4, [r2]
-        str     r3, [r2, #4]
-skip    
-        add     r2, r2, #4
-        sub     r9, r9, #1
-        b       control
-fine    
-        end
+
+               ;       R0 = i
+               ;       R1 = ptr array
+               ;       R4 = in_order
+
+outer_loop     
+               mov     r4,#1
+
+               mov     r1,#size
+               ldr     r0,[r1]
+               mov     r1,#array
+inner_loop     
+               cmp     r0,#1
+               beq     end_inner_loop
+
+               ldr     r2,[r1]
+               ldr     r3,[r1,#4]
+               cmp     r2,r3 ; CMP ARRAY[i], ARRAY[i+1]
+               ble     no_scambio
+
+               str     r3,[r1]
+               str     r2,[r1,#4]
+               mov     R4,#0
+
+no_scambio     
+               add     r1,r1,#4
+               sub     r0,r0,#1
+               b       inner_loop
+
+end_inner_loop 
+               cmp     r4,#0
+               beq     outer_loop
+
+               end
